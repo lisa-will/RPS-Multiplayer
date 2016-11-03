37b32cbf603cd373be989fd8ffa9cec85bc8138b
Lisa Williams <williams.lisarenee@gmail.com>
2016-11-03 09:46:30 -0400

    added firebase

---
+++ logic.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDstr3AJFv3f7PCFbUVjAUZTXCCkvBXNgY",
+    authDomain: "rps-multiplayer-aa18c.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-aa18c.firebaseio.com",
+    storageBucket: "rps-multiplayer-aa18c.appspot.com",
+    messagingSenderId: "591300224724"
+};
+firebase.initializeApp(config);
+
+
